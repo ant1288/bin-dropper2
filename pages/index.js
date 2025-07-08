fae156975f3b575a450ec5147f4d3387905ded6e
ant1288 <anthonyg@opal-olive.com>
2025-07-07 22:46:12 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Bin Dropper</h1>
+      <p>This is your starter Next.js app deployed on Vercel!</p>
+    </div>
+  );
+}
